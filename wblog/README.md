7f32c05f63a317e72bfc54a37d75884d11f55d10
oliveira-sh <0l1ve1r4@proton.me>
2025-04-29 15:53:21 -0300

    trying to add theme submodule

---
+++ wblog/README.md
@@ -0,0 +1,5 @@
+clone:
+
+```sh
+git clone --recurse-submodules https://github.com/oliveira-sh/oliveira-sh
+```
--- wblog/theme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# risotto theme install
-
-git clone https://github.com/joeroe/risotto themes/risotto && cd themes/risotto
-#git checkout $(git tag -l | grep '^v[0-9.]*$' | sort -V | tail -n 1) #is this really needed?
