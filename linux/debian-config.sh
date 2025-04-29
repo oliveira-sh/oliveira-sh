183d6d7a098c52b1f4bc8bba57975e1fd23b1cf6
oliveira-sh <0l1ve1r4@proton.me>
2025-04-29 16:15:45 -0300

    added debian automated install

---
+++ linux/debian-config.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+set -e
+
+# needed
+echo "[*] Apt dependencies"
+sudo apt install curl git -y
+
+# =======================================================================================
+# hyprland, hyprlock, hypridle (not working 29/04/25), rofi-wayland, wallust, gtk-themes 
+# =======================================================================================
+ 
+sh <(curl -L https://raw.githubusercontent.com/JaKooLit/Debian-Hyprland/main/auto-install.sh)
+
+# =======================================================================================
+# Personal oh-my-zsh theme
+# =======================================================================================
+
+mkdir -p "$HOME/.zsh"
+git clone https://github.com/sindresorhus/pure.git "$HOME/.zsh/pure"
+echo "fpath+=($HOME/.zsh/pure)" >> ~/.zshrc
+
+# =======================================================================================
+# Latest Vesktop
+# =======================================================================================
+
+echo "[*] Fetching latest Vesktop .deb URL..."
+DEB_URL=$(curl -sL https://api.github.com/repos/Vencord/Vesktop/releases/latest \
+  | grep "browser_download_url" \
+  | grep ".deb" \
+  | cut -d '"' -f 4)
+
+if [ -z "$DEB_URL" ]; then
+  echo "[!] Failed to find .deb package URL."
+  exit 1
+fi
+
+FILENAME=$(basename "$DEB_URL")
+wget -q --show-progress "$DEB_URL"
+
+echo "[*] Installing $FILENAME..."
+sudo dpkg -i "$FILENAME" || sudo apt-get install -f -y
+
+rm "$FILENAME"
+
+# =======================================================================================
+# Vesktop theme
+# =======================================================================================
+
+curl -L https://github.com/refact0r/system24/blob/main/theme/system24.theme.css > /home/anon/.config/vesktop/system24.theme.css
+
+echo done.
