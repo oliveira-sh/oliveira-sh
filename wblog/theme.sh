99396a10f84d2c75df0a5aab455839877bb6eaca
oliveira-sh <0l1ve1r4@proton.me>
2025-04-29 15:49:58 -0300

    trying to add this shit

--- wblog/theme.sh
+++ wblog/theme.sh
@@ -3,4 +3,4 @@
 # risotto theme install
 
 git clone https://github.com/joeroe/risotto themes/risotto && cd themes/risotto
-git checkout $(git tag -l | grep '^v[0-9.]*$' | sort -V | tail -n 1)
+#git checkout $(git tag -l | grep '^v[0-9.]*$' | sort -V | tail -n 1) #is this really needed?
